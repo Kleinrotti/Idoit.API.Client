aa82bb6fb0735203eab8b5c833d850172b0d77f3
Kleinrotti <kleinrotti@gmail.com>
2021-04-25 14:59:41 +0200

    implemented idoit.reports to library

---
+++ Idoit.API.Client/CMDB/Reports/IdoitReportsInstance.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+
+namespace Idoit.API.Client.CMDB.Reports
+{
+    public class IdoitReportsInstance : IdoitInstanceBase, IReadable<IdoitReportsResponse[]>
+    {
+        public IdoitReportsInstance(IClient myClient) : base(myClient)
+        {
+        }
+
+        private IdoitReportsResponse[] response;
+
+        public IdoitReportsResponse[] Read()
+        {
+            Task t = Task.Run(async () =>
+            {
+                parameter = Client.Parameters;
+                response = await Client.GetConnection().InvokeAsync<IdoitReportsResponse[]>("cmdb.reports.read", parameter);
+            });
+            t.Wait();
+            return response;
+        }
+
+        public IdoitReportsResponse[] Read(int objId)
+        {
+            Task t = Task.Run(async () =>
+            {
+                parameter = Client.Parameters;
+                parameter.Add("id", objId);
+                response = await Client.GetConnection().InvokeAsync<IdoitReportsResponse[]>("cmdb.reports.read", parameter);
+            });
+            t.Wait();
+            return response;
+        }
+    }
+}
---
+++ Idoit.API.Client/CMDB/Reports/IdoitReportsResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace Idoit.API.Client.CMDB.Reports
+{
+    public class IdoitReportsResponse
+    {
+        public int id { get; internal set; }
+        public string category { get; internal set; }
+        public string title { get; internal set; }
+        public string description { get; internal set; }
+        public string created { get; internal set; }
+    }
+}
